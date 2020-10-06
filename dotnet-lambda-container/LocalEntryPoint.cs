2325af62906efb22590543694f17b1a3258bc65d
Sar Malik <malik@sarthak.ca>
2020-10-06 11:50:00 -0500

    reformat: wrap call chain on extension methods, add doc comments

--- dotnet-lambda-container/LocalEntryPoint.cs
+++ dotnet-lambda-container/LocalEntryPoint.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 namespace dotnet_lambda_container
@@ -9,18 +6,22 @@ namespace dotnet_lambda_container
     /// <summary>
     /// The Main function can be used to run the ASP.NET Core application locally using the Kestrel webserver.
     /// </summary>
-    public class LocalEntryPoint
+    public static class LocalEntryPoint
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args)
+                .Build()
+                .Run();
         }
 
+        /// <summary>
+        /// HostBuilder propagates using default startup method
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns>Instance of Host</returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
     }
 }
